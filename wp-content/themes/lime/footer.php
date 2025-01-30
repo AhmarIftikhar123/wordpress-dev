3022448769e29f5ae89e66df580c7e600290e2f9
AhmarIftikhar123 <coadersworldandais@gmail.com>
2025-01-31 01:36:02 +0500

    #86 compleated

--- wp-content/themes/lime/footer.php
+++ wp-content/themes/lime/footer.php
@@ -4,14 +4,44 @@
  *
  * @package Lime
  */
-?>
-<footer id="footer">
-          <h1>Footer</h1>
-          <?php if (is_active_sidebar('footer')):
-                    dynamic_sidebar('footer');
-          endif; ?>
-</footer>
-<?php wp_footer(); ?>
-</body>
 
+?>
+<footer class="site-footer" id="footer">
+    <div class="container color-gray">
+        <div class="row">
+            <section class="col-lg-4 col-md-6 col-sm-12">
+                Some Random Text
+            </section>
+            <section class="col-lg-4 col-md-6 col-sm-12">
+                <?php if (function_exists('is_active_sidebar') && is_active_sidebar('footer')): ?>
+                    <aside><?php dynamic_sidebar('footer'); ?></aside>
+                <?php endif; ?>
+            </section>
+            <section class="col-lg-4 col-md-6 col-sm-12">
+                <ul>
+                    <li><a href="https://check">
+                              <svg width="50">
+                                        <use  href='#checkbox'></use>
+                              </svg>
+                    </a></li>
+                    <li><a href="https://cross">
+                              <svg width="50">
+                                        <use  href='#cross'></use>
+                              </svg>
+                    </a></li>
+                </ul>
+            </section>
+        </div>
+    </div>
+</footer>
+
+<?php 
+if (function_exists('wp_footer')) {
+    wp_footer();
+}
+
+get_template_part('template-parts/content', 'svgs'); 
+?>
+
+</body>
 </html>
